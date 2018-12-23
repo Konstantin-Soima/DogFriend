81952c84e95b0e860d5301191be01373c6050c70
Konstantin Soyma <Konstantin.Soyma@gmail.com>
2018-12-23 21:38:22 +0300

    Initial Commit

---
+++ OhDog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OhDog
+//
+//  Created by Константин Сойма on 23/12/2018.
+//  Copyright © 2018 Константин Сойма. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
